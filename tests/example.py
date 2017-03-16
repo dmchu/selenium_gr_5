b690c5dfd951b4e9489a5132cf0017161fd9fca4
Dmitry Chuveev <dmitry.chuveev@gmail.com>
2017-03-16 22:30:39 +0300

    test login to localhost

--- tests/example.py
+++ tests/example.py
@@ -6,12 +6,22 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
+    wd.implicitly_wait(10)
     request.addfinalizer(wd.quit)
     return wd
 
 def test_example(driver):
+
     driver.get("http://google.com/")
-    driver.find_element_by_name("q").send_keys("webdriver")
+
+    element_q=driver.find_element_by_name("q")
+    # driver.refresh()
+    driver.find_element_by_id("gs_ok0").click()
+    driver.find_element_by_id("K32").click()
+    driver.find_element_by_id("gs_ok0").click()
+
+
+    element_q.send_keys("webdriver")
     driver.find_element_by_name("btnG").click()
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
 
